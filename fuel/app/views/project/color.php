1c88f3d65c52fcc69ff270cb8ecc76563cdb1d2b
ramscuse <jameson.w7281@gmail.com>
2023-03-29 23:10:55 -0600

    m1 started

---
+++ fuel/app/views/project/color.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+        </style>
+    </head>
+
+    <header>
+        <h1>Color Coordinate Generator</h1>
+    </header>
+
+    <body>
+        <div>
+
+        </div>
+    </body>
+
+    <footer>
+        <h3>LOCAL FUEL</h3>
+    </footer>
+</html>
